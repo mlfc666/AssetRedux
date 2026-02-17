2a6c75631fb3c7d07dd1aceb9f0510a4a7b7bc2b
梦落凡尘 <1323230180@qq.com>
2026-02-17 13:51:53 +0800

    feat(core): 迁移到 .NET 6 并实现插件基础架构
    - 将目标框架从 netstandard2.1 升级到 net6.0
    - 添加了对 BepInEx Unity IL2CPP 的支持
    - 实现了插件入口点 Plugin.cs
    - 添加了 AssetController 组件注册机制
    - 集成了 Harmony 补丁系统
    - 配置了 IL2CPP 互操作性支持
    - 设置了项目构建属性和依赖项引用

--- AssetRedux/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace AssetRedux
-{
-    public class Class1
-    {
-    }
-}
---
+++ AssetRedux/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using AssetRedux.Components;
+using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using Il2CppInterop.Runtime.Injection;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace AssetRedux;
+
+[BepInPlugin(PluginInfo.PluginGuid, PluginInfo.PluginName, PluginInfo.PluginVersion)]
+public class Plugin : BasePlugin
+{
+    public new static ManualLogSource Log = null!;
+
+    public override void Load()
+    {
+        // 强制控制台 UTF-8 (解决中文乱码)
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Log = base.Log;
+
+        // 注册 IL2CPP 类型 
+        ClassInjector.RegisterTypeInIl2Cpp<AssetController>();
+
+        // 创建控制器
+        var go = new GameObject(PluginInfo.ControllerName);
+        Object.DontDestroyOnLoad(go);
+        go.hideFlags = HideFlags.HideAndDontSave; // 防止被游戏内的清理脚本误删
+
+        // 挂载脚本
+        go.AddComponent<AssetController>();
+
+        // 应用 Harmony 补丁
+        // PatchAll 比较耗时，放在最后，确保前面关键组件已就绪
+        new Harmony(PluginInfo.PluginGuid).PatchAll();
+    }
+}
+
+public static class PluginInfo
+{
+    public const string PluginGuid = "moe.mlfc.assetredux";
+    public const string PluginName = "MlfcAssetRedux";
+    public const string PluginVersion = "0.0.1";
+    public const string ControllerName = PluginName + "Controller";
+    public const string TargetVersion = PluginVersion;
+}
